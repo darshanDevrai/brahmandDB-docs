a81c759c2743b34743e53c7e0ea1103e10ee0f33
darshanDevrai <darshan.21devrai@gmail.com>
2025-05-24 19:05:57 -0400

    feat: initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,41 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://darshanDevrai.github.io/brahmand-docs',
+  	// base: 'brahmand-docs',
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'BrahmandDB',
+			// customCss: [
+			// 	// Relative path to your custom CSS file
+			// 	'./src/styles/custom.css',
+			//   ],
+			  logo: {
+				src: './public/logo.svg',
+			  },
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
-					label: 'Guides',
+                    label: 'Home',
+                    link: '/home',
+                },
+				{
+					label: 'Install BrahmandDB',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Installation', slug: 'installation/install' },
 					],
 				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+				// {
+				// 	label: 'Guides',
+				// 	items: [
+				// 		// Each item here is one entry in the navigation menu.
+				// 		{ label: 'Example Guide', slug: 'guides/example' },
+				// 	],
+				// },
+				// {
+				// 	label: 'Reference',
+				// 	autogenerate: { directory: 'reference' },
+				// },
 			],
 		}),
 	],
